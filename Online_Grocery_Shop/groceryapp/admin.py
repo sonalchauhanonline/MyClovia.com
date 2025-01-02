139d2be0b3105466511fd8de4d623b0c414ecb6f
sonalchauhanonline <70689040+sonalchauhanonline@users.noreply.github.com>
2025-01-02 20:07:39 +0530

    Add files via upload

---
+++ Online_Grocery_Shop/groceryapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from groceryapp.models import Students
+
+# Register your models here.
+admin.site.register(Students)
---
+++ Online_Grocery_Shop/groceryapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+
+    return render(request,'home.html')
+
+
+def index(request):
+    return render(request, 'navigation.html')
+
+
+def about(request):
+    return render (request,'about.html')
